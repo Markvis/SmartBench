d93788941ad2acb7a729dabd8564baddf9c5168c
MarkVis <favismark@gmail.com>
2015-08-26 14:14:50 -0700

    -implemented asynctask on both multithread and single thread benchmark -included a lock on the processing to prevent creating to many threads

--- app/src/main/java/com/asuscomm/geniusware/smartbench/cpubenchmark.java
+++ app/src/main/java/com/asuscomm/geniusware/smartbench/cpubenchmark.java
@@ -11,11 +11,15 @@ import android.widget.TextView;
 public class cpubenchmark extends AppCompatActivity {
 
     final int primeCount = 100000;
+    boolean inProgress = false;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpubenchmark);
+
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
     }
 
     @Override
@@ -42,69 +46,21 @@ public class cpubenchmark extends AppCompatActivity {
 
     public void onClickMultiThread(View v)
     {
-//        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
-//        final Prime y = new Prime();
-//        final int processPerThread = primeCount/Runtime.getRuntime().availableProcessors();
-//        final int numberOfProcessors = Runtime.getRuntime().availableProcessors();
-//        Thread []threads = new Thread[numberOfProcessors];
-//
-//        // initialize threads
-//        for(int i = 0; i < numberOfProcessors; i++) {
-//            final int count = i;
-//            threads[i] = new Thread(new Runnable() {
-//                public void run() {
-//                    y.totalPrimesIn(processPerThread*count,processPerThread*(count+1));
-//                }
-//            });
-//            //threads[i].start();
-//        }
-//
-//        // start timer
-//        long time = System.currentTimeMillis();
-//
-//        // start threads
-//        for(int i = 0; i < numberOfProcessors; i++) {
-//            threads[i].start();
-//        }
-//
-//        // wait till all threads are complete
-//        for(int i = 0; i < numberOfProcessors; i++) {
-//            try {
-//                threads[i].join();
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        // end timer
-//        long res = System.currentTimeMillis() - time;
-//
-//        // write to screen
-//        TextView result = (TextView) findViewById(R.id.resultTextView);
-//        result.setText(String.valueOf(res + " ms"));
-
-        new multiThreadAsync().execute("");
+        if(!inProgress) {
+            inProgress = true;
+            new multiThreadBenchmarkAsync().execute("");
+        }
     }
 
     public void onClickSingleThread(View v)
     {
-        final Prime y = new Prime();
-
-        // start timer
-        long time = System.currentTimeMillis();
-
-        // Single Thread
-        y.totalPrimesIn(0,primeCount);
-
-        // end timer
-        long res = System.currentTimeMillis() - time;
-
-        // write to screen
-        TextView result = (TextView) findViewById(R.id.resultTextView);
-        result.setText(String.valueOf(res + " ms"));
+        if(!inProgress) {
+            inProgress = true;
+            new singleThreadBenchmarkAsync().execute("");
+        }
     }
 
-    private class multiThreadAsync extends AsyncTask<String, Void,String>{
+    private class multiThreadBenchmarkAsync extends AsyncTask<String, Void,String>{
 
         @Override
         protected String doInBackground(String... params) {
@@ -122,7 +78,6 @@ public class cpubenchmark extends AppCompatActivity {
                         y.totalPrimesIn(processPerThread*count,processPerThread*(count+1));
                     }
                 });
-                //threads[i].start();
             }
 
             // start timer
@@ -149,31 +104,38 @@ public class cpubenchmark extends AppCompatActivity {
         }
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+        protected void onPostExecute(String s) {
+            resultTextView.setText(String.valueOf(s + " ms"));
+            inProgress = false;
+
+            super.onPostExecute(s);
+        }
+    }
+    private class singleThreadBenchmarkAsync extends AsyncTask<String, Void,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            final Prime y = new Prime();
+
+            // start timer
+            long time = System.currentTimeMillis();
+
+            // Single Thread
+            y.totalPrimesIn(0,primeCount);
+
+            // end timer
+            long res = System.currentTimeMillis() - time;
+
+            return String.valueOf(res);
         }
 
         @Override
         protected void onPostExecute(String s) {
-            TextView result = (TextView) findViewById(R.id.resultTextView);
-            result.setText(String.valueOf(s + " ms"));
+            resultTextView.setText(String.valueOf(s + " ms"));
+            inProgress = false;
 
             super.onPostExecute(s);
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onCancelled(String s) {
-            super.onCancelled(s);
-        }
-
-        @Override
-        protected void onCancelled() {
-            super.onCancelled();
         }
     }
 }
