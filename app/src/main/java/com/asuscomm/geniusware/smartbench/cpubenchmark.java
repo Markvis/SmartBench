ecba2ca50f2d738c9bf7d6e1c539bb1b6bf405cb
MarkVis <favismark@gmail.com>
2015-08-26 12:27:55 -0700

    -modified multithreaded benchmark implementation to use AsyncTask

--- app/src/main/java/com/asuscomm/geniusware/smartbench/cpubenchmark.java
+++ app/src/main/java/com/asuscomm/geniusware/smartbench/cpubenchmark.java
@@ -1,5 +1,6 @@
 package com.asuscomm.geniusware.smartbench;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -42,40 +43,47 @@ public class cpubenchmark extends AppCompatActivity {
     public void onClickMultiThread(View v)
     {
 //        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
-        final Prime y = new Prime();
-        final int processPerThread = primeCount/Runtime.getRuntime().availableProcessors();
-        final int numberOfProcessors = Runtime.getRuntime().availableProcessors();
-        Thread []threads = new Thread[numberOfProcessors];
+//        final Prime y = new Prime();
+//        final int processPerThread = primeCount/Runtime.getRuntime().availableProcessors();
+//        final int numberOfProcessors = Runtime.getRuntime().availableProcessors();
+//        Thread []threads = new Thread[numberOfProcessors];
+//
+//        // initialize threads
+//        for(int i = 0; i < numberOfProcessors; i++) {
+//            final int count = i;
+//            threads[i] = new Thread(new Runnable() {
+//                public void run() {
+//                    y.totalPrimesIn(processPerThread*count,processPerThread*(count+1));
+//                }
+//            });
+//            //threads[i].start();
+//        }
+//
+//        // start timer
+//        long time = System.currentTimeMillis();
+//
+//        // start threads
+//        for(int i = 0; i < numberOfProcessors; i++) {
+//            threads[i].start();
+//        }
+//
+//        // wait till all threads are complete
+//        for(int i = 0; i < numberOfProcessors; i++) {
+//            try {
+//                threads[i].join();
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        // end timer
+//        long res = System.currentTimeMillis() - time;
+//
+//        // write to screen
+//        TextView result = (TextView) findViewById(R.id.resultTextView);
+//        result.setText(String.valueOf(res + " ms"));
 
-        // start timer
-        long time = System.currentTimeMillis();
-
-        // Multithreading
-        for(int i = 0; i < numberOfProcessors; i++) {
-            final int count = i;
-            threads[i] = new Thread(new Runnable() {
-                public void run() {
-                    y.totalPrimesIn(processPerThread*count,processPerThread*(count+1));
-                }
-            });
-            threads[i].start();
-        }
-
-        // wait till all threads are complete
-        for(int i = 0; i < numberOfProcessors; i++) {
-            try {
-                threads[i].join();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        // end timer
-        long res = System.currentTimeMillis() - time;
-
-        // write to screen
-        TextView result = (TextView) findViewById(R.id.resultTextView);
-        result.setText(String.valueOf(res + " ms"));
+        new multiThreadAsync().execute("");
     }
 
     public void onClickSingleThread(View v)
@@ -94,5 +102,78 @@ public class cpubenchmark extends AppCompatActivity {
         // write to screen
         TextView result = (TextView) findViewById(R.id.resultTextView);
         result.setText(String.valueOf(res + " ms"));
+    }
+
+    private class multiThreadAsync extends AsyncTask<String, Void,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            final Prime y = new Prime();
+            final int processPerThread = primeCount/Runtime.getRuntime().availableProcessors();
+            final int numberOfProcessors = Runtime.getRuntime().availableProcessors();
+            Thread []threads = new Thread[numberOfProcessors];
+
+            // initialize threads
+            for(int i = 0; i < numberOfProcessors; i++) {
+                final int count = i;
+                threads[i] = new Thread(new Runnable() {
+                    public void run() {
+                        y.totalPrimesIn(processPerThread*count,processPerThread*(count+1));
+                    }
+                });
+                //threads[i].start();
+            }
+
+            // start timer
+            long time = System.currentTimeMillis();
+
+            // start threads
+            for(int i = 0; i < numberOfProcessors; i++) {
+                threads[i].start();
+            }
+
+            // wait till all threads are complete
+            for(int i = 0; i < numberOfProcessors; i++) {
+                try {
+                    threads[i].join();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            // end timer
+            long res = System.currentTimeMillis() - time;
+
+            return String.valueOf(res);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView result = (TextView) findViewById(R.id.resultTextView);
+            result.setText(String.valueOf(s + " ms"));
+
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
     }
 }
